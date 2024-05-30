167d1cc9c2b31fdb8f239897daeba442ee904576
Manasvi713 <manasvisingh713@gmail.com>
2024-05-30 17:18:27 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage';
 import AdminPage from './Components/AdminPage';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
 
 
 function App() {
   return (
     <div className='App'>
-      {/* <HomePage /> */}
-      <AdminPage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginSignup/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/admin" element={<AdminPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
