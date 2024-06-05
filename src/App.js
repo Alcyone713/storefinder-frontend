91308a849c9aee374c00666fa2ddd9f524e9ad81
Manasvi713 <manasvisingh713@gmail.com>
2024-06-05 20:08:52 +0530

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.css';
-import HomePage from './Components/HomePage';
-import AdminPage from './Components/AdminPage';
-import FavouriteStores from './Components/FavouriteStores';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
-
+import React from "react";
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import AdminPage from "./Components/AdminPage";
+import FavouriteStores from "./Components/FavouriteStores";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+import ProtectedRoutes from "./Components/ProtectedRoutes";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LoginSignup/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/admin" element={<AdminPage/>}/>
-          <Route path="/favourite" element={<FavouriteStores/>}/>
+          <Route path="/" element={<LoginSignup />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/favourite" element={<FavouriteStores />} />
+          </Route>
         </Routes>
       </Router>
     </div>
